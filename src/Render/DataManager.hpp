87c40cb871ddc1bb5de1ea8f7e4cd9a62a6e4b96
Jonathan Fritsch <jonathan.fritsch@dlr.de>
2020-11-10 08:54:08 +0100

    :memo: Add docs for DataManager

--- src/Render/DataManager.hpp
+++ src/Render/DataManager.hpp
@@ -43,23 +43,42 @@ class DataManager {
 
   enum class VolumeFileType { eInvalid = -1, eGaia, eVtk };
 
+  /// Create a data manager reading files matching "filenamePattern" from the directory "path".
+  /// The manager will use an appropriate reader for the given file type.
   DataManager(std::string path, std::string filenamePattern, VolumeFileType type);
   ~DataManager();
 
-  cs::utils::Property<std::vector<int>>         pTimesteps;
+  /// List of timesteps for which files were found.
+  cs::utils::Property<std::vector<int>> pTimesteps;
+  /// List of available scalars in the data.
   cs::utils::Property<std::vector<std::string>> pScalars;
 
+  /// Until this returns true queried data will have no active scalar.
   bool isReady();
 
+  /// Sets the current timestep to the given value.
+  /// Future calls to getData() will return data for this time.
   void setTimestep(int timestep);
+  /// Loads data for the timestep into the cache.
+  /// Does not change the current timestep.
   void cacheTimestep(int timestep);
+  /// Sets the current scalar to the given value.
+  /// Has to be one of the values in pScalars.
+  /// Future calls to getData() will return data with this as the active scalar.
+  void setActiveScalar(std::string scalar);
+  /// Returns whether the current state changed since the last call to getData().
   bool isDirty();
 
-  void setActiveScalar(std::string scalar);
-
+  /// Returns the data for the current state.
+  /// May block, if the requested data is not yet loaded.
+  /// If isReady() returns false, the data will have no active scalar.
   vtkSmartPointer<vtkDataSet> getData();
+  /// Returns the data for the given state.
+  /// May block, if the requested data is not yet loaded.
   vtkSmartPointer<vtkDataSet> getData(State state);
-  State                       getState();
+  /// Returns the current state.
+  /// If isReady() returns false, the scalar won't be set yet.
+  State getState();
 
  private:
   VolumeFileType mType;
