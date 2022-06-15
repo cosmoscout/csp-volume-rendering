d0db76f978dd5321848aefde03bdf5bd2804cbf7
Markus Flatken <markus.flatken@dlr.de>
2022-06-15 12:51:06 +0200

    :wrench: Tweak deafult animation speed

--- gui/js/csp-volume-rendering.js
+++ gui/js/csp-volume-rendering.js
@@ -15,7 +15,7 @@
      */
     init() {
       CosmoScout.gui.initInputs();
-      CosmoScout.gui.initSlider("volumeRendering.setAnimationSpeed", 1, 100, 1, [10]);
+      CosmoScout.gui.initSlider("volumeRendering.setAnimationSpeed", 1, 100, 1, [20]);
       CosmoScout.gui.initSlider("volumeRendering.setResolution", 32, 2048, 32, [256]);
       CosmoScout.gui.initSliderRange("volumeRendering.setSamplingRate",
           {"min": 0.001, "25%": 0.01, "50%": 0.1, "75%": 1, "max": 10}, [0.005]);
